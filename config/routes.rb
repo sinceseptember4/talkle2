ff993b4faeee4326eceb87b75fa853e406877743
EC2 Default User <ec2-user@ip-172-31-7-71.ap-northeast-1.compute.internal>
2020-12-15 11:28:15 +0000

    made mypages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :tweets  do
     resources :comments ,only: [:create]
   end
-  resources :users,only: [:show]
+  resources :users,only: [:show,:edit,:update]
   
 end
